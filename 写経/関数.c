8f2304290339a7678c229e21cf10b122aaa49e2d
daisuke harato <daikon0313@yahoo.co.jp>
2023-01-01 21:10:39 +0900

    2023/01/01

--- 写経/関数.c
+++ 写経/関数.c
@@ -129,3 +129,157 @@ int calc_year(int year)
 	}
 }
 
+
+// sample
+#include <stdio.h>
+
+int countfunc(void);
+
+int main(void)
+{
+    countfunc();
+    countfunc();
+    countfunc();
+    return 0;
+}
+
+int countfunc(void)
+{
+    int count = 0;
+    count ++;
+    printf("%d\n", count);
+    return count;
+}
+
+// block
+#include <stdio.h>
+
+int main(void)
+{
+    int value1 = 10;
+    int value2 = 20;
+
+    printf("1:value1 %d\n", value1);
+    printf("1:value2 %d\n", value2);
+
+    {
+        int value1;
+        value1 = 30;
+        value2 = 40;
+        printf("2:value1 %d\n", value1);
+        printf("2:value2 %d\n", value2);
+    }
+
+    printf("3:value1 %d\n", value1);
+    printf("3:value2 %d\n", value2);
+
+    return 0;
+}
+
+/*
+出力:
+1:value1 10
+1:value2 20
+2:value1 30
+2:value2 40
+3:value1 10
+3:value2 40
+*/
+
+//global変数
+//global変数は初期化せずにとも0から始まる
+#include <stdio.h>
+
+int count; // グローバル変数
+
+int countfunc(void); //プロトタイプ宣言
+
+int main(void)
+{
+    countfunc();
+    countfunc();
+    countfunc();
+    return 0;
+}
+
+int countfunc(void)
+{
+    count++;
+    print("%d\n", count);
+    return count;
+}
+
+//
+#include <stdio.h>
+
+int count; // グローバル変数
+
+int countfunc(void); //プロトタイプ宣言
+
+int main(void)
+{
+    countfunc();
+    count = 10; //修正を行う
+    countfunc();
+    countfunc();
+    return 0;
+}
+
+int countfunc(void)
+{
+    count++;
+    printf("%d\n", count);
+    return count;
+}
+
+// グローバル関数とローカル関数で同名宣言できる
+// グローバル関数よりもローカル関数を優先する
+#include <stdio.h>
+
+int count; // グローバル変数
+
+int countfunc(void); //プロトタイプ宣言
+
+int main(void)
+{
+    int count; //変数を定義
+
+    countfunc();
+    count = 10; //修正を行う
+    countfunc();
+    countfunc();
+    printf("main : count = %d\n", count);
+    return 0;
+}
+
+int countfunc(void)
+{
+    count++;
+    printf("%d\n", count);
+    return count;
+}
+
+//static variables
+//static プログラムが終了するまでの残る
+//       自動的に0に初期化される
+#include <stdio.h>
+
+int countfunc(void);
+
+int main (void)
+{
+    countfunc();
+    countfunc();
+    countfunc();
+    return 0;
+}
+
+int countfunc(void)
+{
+    static int count; //静的なローカル変数
+
+    count++;
+    printf("%d\n", count);
+
+    return count;
+}
