c8ca4a45404d8e814baee45026c4a7338d7e89aa
daisuke harato <daikon0313@yahoo.co.jp>
2023-01-03 20:43:09 +0900

    2023/01/03

--- APG4b/ch1/output_comment.c
+++ APG4b/ch1/output_comment.c
@@ -1,5 +1,50 @@
+// basic
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
+    // cout に << でメッセージを送る
+    cout << "Hello World" << endl;
+}
+
+// muliti output
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // endl がないため改行されない
+    cout << "a";
+    cout << "b" << endl;
+    // cd と出力
+    cout << "c" << "d" << endl;
+}
+
+// number output
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // ""を使わずにかける
+    cout << 2525 << endl;
+}
+
+// comment
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  // cout << "Hello, world!" << endl; //Hello, worldと表示
+  /*ここも
+  コメント*/
+}
+
+
+// test
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cout << "こんにちは" << endl;
+  cout << "AtCoder" << endl;
 }
